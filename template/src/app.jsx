78d38b81bb40f14f8d2a856dcb46e2f7d1b80462
Aditya Vaish <adivaish@microsoft.com>
2019-11-26 13:14:26 -0800

    update

--- template/src/app.jsx
+++ template/src/app.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable react/prefer-stateless-function */
 
-import { Component, h, render } from 'preact';
+import { Component, h, render } from "preact";
 
 export class App extends Component {
-	render() {
-		return <div>Hello World!</div>;
-	}
+  render() {
+    return (
+      <div className="spl">
+        Welcome to the world of developing Bing homepage special experience!
+      </div>
+    );
+  }
 }
 
-if (typeof window !== 'undefined' && window.document) {
-	render(<App />, document.querySelector('.hp_special_experience'));
+if (
+  typeof window !== "undefined" &&
+  window.document &&
+  location.href.indexOf("search") < 0
+) {
+  const elem = document.createElement("div");
+  (document.querySelector(".hp_body") || document.body).appendChild(elem);
+  render(<App />, elem);
 }
