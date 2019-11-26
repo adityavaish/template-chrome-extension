1b20cb9f5202240d565b53f4ccc25a9c21058180
Aditya Vaish <adivaish@microsoft.com>
2019-11-25 16:56:40 -0800

    update

--- template/src/extension.js
+++ template/src/extension.js
@@ -1,8 +1,8 @@
 const style = document.createElement('link');
-style.href = 'http://localhost:8080/style.css';
+style.href = 'http://127.0.0.1:8080/style.css';
 style.rel = 'stylesheet';
 document.head.appendChild(style);
 
 const script = document.createElement('script');
-script.src = 'http://localhost:8080/bundle.js';
+script.src = 'http://127.0.0.1:8080/bundle.js';
 document.body.appendChild(script);
